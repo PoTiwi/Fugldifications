0e8d80b19413075c20846a849a77f283167b9f4f
Fries <freshfrenchfries@proton.me>
2026-08-20 10:02:20 +0200

    Update src-main.cpp

--- source/src-main.cpp
+++ source/src-main.cpp
@@ -1,24 +1,18 @@
 // here where you would edit. main.cpp bascially
 
-#include "inserts/Memory.hpp"
+#include "Backbone/Memory.hpp"
 
 int main() {
     if (!init()) return 1;
 
     Instance gravityAddr = Imem.point_addr(g_world, Offsets::World::Gravity);
 
-    while (g_isRunning) {
-        if (checkExitKey()) {
-            g_isRunning = false;
-            break;
-        }
-
+    while_instance_running {
+        
         if (gravityAddr) {
             // Rewrite the memory to set the game gravity to 0.0 (float)
             Imem.overwrite<float>(gravityAddr, 0.0f);
         }
-
-        sleep_ms(10); 
     }
 
     cleanup();
